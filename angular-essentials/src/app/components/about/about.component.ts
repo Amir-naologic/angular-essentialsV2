09708d62f3deb38ee127c7c40a83261c3a60f402
Amir H <amir.h@naologic.com>
2025-03-19 17:37:32 +0100

    Week 2, ValidationDirective (Custom Form Validator) implementation

--- angular-essentials/src/app/components/about/about.component.ts
+++ angular-essentials/src/app/components/about/about.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DraggableDirective } from "../../shared/Draggable.directive";
+import {
+  AbstractControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators, FormControl
+} from "@angular/forms";
+
+/**
+ * Custom validator that checks if the password and confirmPassword match.
+ */
+export const MatchingValuesValidator = (password: string, confirmPass: string): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const pass = control.get(password);
+    const confirmPassword = control.get(confirmPass);
+
+    // -->Check: if both password and confirmPassword are provided and a match
+    if (pass && confirmPassword && pass.value === confirmPassword.value) {
+      return null;
+    }
+
+    // -->Return: mismatch error if the values do not match
+    return { valuesMismatch: true };
+  }
+};
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, DraggableDirective],
+  imports: [CommonModule, DraggableDirective, ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+
+export class AboutComponent {
+
+  /**
+   * Init the password form group with password and confirmPassword controls.
+   */
+  passwordForm: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  }, [MatchingValuesValidator('password', 'confirmPassword')]);
+
+  /**
+   * Logs the form.
+   */
+  public onSubmit(): void {
+    console.log(this.passwordForm.value);
+  }
+
+  /**
+   * Getter to check if the passwords do not match.
+   */
+  public get passwordMismatch(): boolean {
+    return this.passwordForm.hasError('valuesMismatch');
+  }
+}
