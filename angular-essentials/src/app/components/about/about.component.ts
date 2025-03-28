ce702ac0a52833b22e9d95d9ad3101c00973e49b
Amir H <amir.h@naologic.com>
2025-03-28 16:32:03 +0100

    Week 4 Enhancements v2

--- angular-essentials/src/app/components/about/about.component.ts
+++ angular-essentials/src/app/components/about/about.component.ts
@@ -2,41 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DraggableDirective } from '../../shared/directives/draggable.directive';
 import { TranslateValidationErrorsPipe } from "../../shared/pipes/translate-validation-errors.pipe";
+import { MatchingValuesValidator } from "../../shared/utils/utils";
 import {
-  AbstractControl,
   FormGroup,
   ReactiveFormsModule,
-  ValidationErrors,
-  ValidatorFn,
   Validators, FormControl
 } from "@angular/forms";
 
-/**
- * Custom validator that checks if the password and confirmPassword match.
- */
-export function MatchingValuesValidator(controlName: string, matchingControlName: string): ValidatorFn {
-  return (formGroup: AbstractControl): ValidationErrors | null => {
-    // -->Get: Form controls (fields)
-    const control = formGroup.get(controlName);
-    const matchingControl = formGroup.get(matchingControlName);
-
-    // -->Skip: valuesMismatch validation if other validations exist
-    if (matchingControl?.errors && !matchingControl.errors['valuesMismatch']) {
-      return null;
-    }
-
-    // -->Check: if both fields are not a match
-    if (control?.value !== matchingControl?.value) {
-      // -->Set: valuesMismatch error
-      matchingControl?.setErrors({valuesMismatch: true});
-    }else{
-      matchingControl?.setErrors(null);
-    }
-
-    // -->Clear: valuesMismatch error if values match
-    return null;
-  };
-}
 @Component({
   selector: 'app-about',
   imports: [CommonModule, DraggableDirective, ReactiveFormsModule, TranslateValidationErrorsPipe],
