edd3989c3b27c13334c4cedf529fc716db5924d1
Amir H <amir.h@naologic.com>
2025-04-07 09:46:38 +0200

    Improvements to try to make design more one-to-one

--- angular-essentials/src/app/shared/kanban-design-components/group-insertion/add-group.component.ts
+++ angular-essentials/src/app/shared/kanban-design-components/group-insertion/add-group.component.ts
@@ -15,4 +15,14 @@ export class AddGroupComponent {
     '#E0FFFF', '#D6EFFF', '#E7E7FB', '#ECDDFB',
     '#F5F5F5', '#DFF8E8', '#E1E7F8', '#E1FFFF'
   ];
+
+  public hexToRgba(hex: string, alpha: number): string {
+    console.log({hex});
+    const bigint = parseInt(hex.replace('#', ''), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
 }
